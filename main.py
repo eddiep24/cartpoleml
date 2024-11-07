c6a6abee4e4ae1cfd451bbb02148460a20992a2c
Eddie Phillips <eddiephillips101@gmail.com>
2024-11-07 17:49:49 -0600

    Added main

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+
+Main implmenetation that trains a fully connected neural network using backpropogation and gets feedback via cart pole RL.
+
+"""
+
+
+def main():
+  pass
+
+
+
+if __name__ == "__main__":
+  main()
